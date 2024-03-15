22bf7f56e40290d5879166f653c45546d8f12aca
AneelaTasneem <aneelatasneem90@gmail.com>
2024-03-15 06:37:21 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+// // String Concatenation:
+// // Storing names in variables:
+// let firstName: String = ' Aneela';
+// let lastName: string = 'Tasneem';
+// let fullName: string = firstName  +  '  '  + lastName;
+// //  Printing full name:
+// console.log(fullName);
+// Template Literals:
+// Storing names in variables:
+let firstName = ' Muhammad';
+let lastName = 'Shaban';
+let fullName = `${firstName} ${lastName}`;
+//  Printing full name:
+console.log(fullName);
---
+++ main.ts
@@ -0,0 +1,20 @@
+
+// // String Concatenation:
+
+// // Storing names in variables:
+// let firstName: String = ' Aneela';
+// let lastName: string = 'Tasneem';
+// let fullName: string = firstName  +  '  '  + lastName;
+
+// //  Printing full name:
+// console.log(fullName);
+
+
+// // Template Literals:
+// // Storing names in variables:
+// let firstName: String = ' Muhammad';
+// let lastName: string = 'Shaban';
+// let fullName: string = `${firstName} ${lastName}`;
+
+// //  Printing full name:
+// console.log(fullName);
